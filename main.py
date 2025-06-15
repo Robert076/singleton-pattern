ef22951912560171e1eb75992dad33e5c875c32a
Robert076 <robertberes06@gmail.com>
2025-06-15 08:03:27 +0300

    done

---
+++ main.py
@@ -0,0 +1,20 @@
+class AppState:
+    instance = None
+    
+    def __init__(self):
+        self.isLoggedIn = False
+        
+    @staticmethod
+    def getAppState():
+        if not AppState.instance:
+            AppState.instance = AppState()
+        return AppState.instance
+    
+appState1 = AppState.getAppState()
+print(appState1.isLoggedIn) # False
+
+appState2 = AppState.getAppState()
+appState1.isLoggedIn = True
+
+print(appState1.isLoggedIn) # True
+print(appState2.isLoggedIn) # True
